771be553e4653c3c8d4664862ef20219ffe9b8fb
fluxfw <fluxfw@fluxlabs.ch>
2022-03-28 12:59:19 +0200

    server

---
+++ src/Adapter/Server/ScormPlayerRestApiServerRouteCollector.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace FluxScormPlayerRestApi\Adapter\Server;
+
+use FluxScormPlayerRestApi\Adapter\Route\AssetRoute;
+use FluxScormPlayerRestApi\Adapter\Route\DeleteRoute;
+use FluxScormPlayerRestApi\Adapter\Route\GetDataRoute;
+use FluxScormPlayerRestApi\Adapter\Route\PlayRoute;
+use FluxScormPlayerRestApi\Adapter\Route\PostDataRoute;
+use FluxScormPlayerRestApi\Adapter\Route\StaticRoute;
+use FluxScormPlayerRestApi\Adapter\Route\UploadRoute;
+use FluxScormPlayerRestApi\Libs\FluxRestApi\Collector\RouteCollector;
+use FluxScormPlayerRestApi\Libs\FluxScormPlayerApi\Adapter\Api\ScormPlayerApi;
+
+class ScormPlayerRestApiServerRouteCollector implements RouteCollector
+{
+
+    private function __construct(
+        private readonly ScormPlayerApi $scorm_player_api
+    ) {
+
+    }
+
+
+    public static function new(
+        ScormPlayerApi $scorm_player_api
+    ) : static {
+        return new static(
+            $scorm_player_api
+        );
+    }
+
+
+    public function collectRoutes() : array
+    {
+        return [
+            AssetRoute::new(
+                $this->scorm_player_api
+            ),
+            DeleteRoute::new(
+                $this->scorm_player_api
+            ),
+            GetDataRoute::new(
+                $this->scorm_player_api
+            ),
+            PlayRoute::new(
+                $this->scorm_player_api
+            ),
+            PostDataRoute::new(
+                $this->scorm_player_api
+            ),
+            StaticRoute::new(
+                $this->scorm_player_api
+            ),
+            UploadRoute::new(
+                $this->scorm_player_api
+            )
+        ];
+    }
+}
